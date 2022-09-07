c4157bfef1650e43036a8d56da0795db85e56da7
xauvk <xauvkubalexanderyang@live.com>
2022-09-07 13:23:02 -0400

    commit tree 9f29c5e334c49149573775cc3ae8570f75361833

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    validate :title_not_clickbaity
+
+    def title_not_clickbaity
+        if title.include "Won't Believe"
+            errors.add(:title, "title is clickbait")
+        elsif title.include "Secret"
+            errors.add(:title, "title is clickbait")
+        elsif title.include "Top"
+            errors.add(:title, "title is clickbait")
+        elsif title.include "Guess"
+            errors.add(:title, "title is clickbait")
+        end
+    end
+    
 end
